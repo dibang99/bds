1734963c99e5b4ee9ee9c2fac4e2ffe0f6084377
dibang99 <dibang99@gmail.com>
2021-04-23 19:48:45 +0700

    add gio hang

--- __manifest__.py
+++ __manifest__.py
@@ -8,12 +8,18 @@
     'category': 'Uncategorized',
     'version': '0.1',
     'depends': [
-        'product'
+        'product',
+        'sale',
+        'website',
+        'website_form',
+        'digest',
+        'website_mail',
     ],
     'data': [
         'security/ir.model.access.csv',
         'views/product_views.xml',
-        'views/product_template_views.xml'
+        'views/product_template_views.xml',
+        'views/sale_order_views.xml',
     ],
     # 'qweb': ['static/src/xml/*.xml'],
     'installable': True,
